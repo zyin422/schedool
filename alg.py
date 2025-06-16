79721b6b2407a567da3dfcdafc3549bfce6cd4bf
Zixuan Yin <yinzixuansz@gmail.com>
2025-06-16 15:38:35 -0700

    Created app.py

---
+++ alg.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, subjects):
+        self.subjects = subjects
+        
+        
+class Teacher:
+    def __init__(self, subjects, periods):
+        self.subjects = subjects
+        self.periods = periods
+        
